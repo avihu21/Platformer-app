a9a883461d96e87cae2879d66ab2f8785529b6da
avihu <avihuelia@gmail.com>
2019-05-25 16:30:46 +0300

    Major bug fix in the GameActivity class

--- app/src/main/java/com/tegi/platformer/GameActivity.java
+++ app/src/main/java/com/tegi/platformer/GameActivity.java
@@ -1,13 +1,35 @@
 package com.tegi.platformer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
-public class GameActivity extends AppCompatActivity {
+class GameActivity extends Activity {
+
+    GameEngine mGameEngine;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+
+        Point size = new Point();
+        display.getSize(size);
+        mGameEngine = new GameEngine(this,size);
+        setContentView(mGameEngine);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mGameEngine.startThread();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mGameEngine.stopThread();
     }
 }
