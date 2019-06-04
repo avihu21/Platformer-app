8f70dd7d347b716c83285c0718e1eaaa1630c96e
avihu <avihuelia@gmail.com>
2019-06-04 13:20:55 +0300

    PhysicsEngine class p1

--- app/src/main/java/com/tegi/platformer/PhysicsEngine.java
+++ app/src/main/java/com/tegi/platformer/PhysicsEngine.java
@@ -14,7 +14,27 @@ class PhysicsEngine {
     }
 
     private void detectCollisions(GameState gs,ArrayList<GameObject> objects){
-        //more code coming soon
+        boolean collisionOccurred = false;
+
+        //something collides with some part of the player most frames
+        //here are some handy references get a reference to the players position
+        //because there will probably a need to update it
+        Transform playerTransform = objects.get(LevelManager.PLAYER_INDEX).getTransform();
+        PlayerTransform playerPlayerTransform = (PlayerTransform) playerTransform;
+
+        //get the players extra coliders from the cast objects
+        ArrayList<RectF> playerColliders = playerPlayerTransform.getColliders();
+        PointF playerLocation = playerTransform.getLocation();
+
+        for (GameObject go: objects){
+            //just need to check player collisions with everything else
+            if (go.checkActive()){
+                //object is active so check for collision with player - anywhere at all
+                if (RectF.intersects(go.getTransform().getCollider(),playerTransform.getCollider())){
+                    //a collision of some kind has occurred 
+                }
+            }
+        }
     }
 
 
